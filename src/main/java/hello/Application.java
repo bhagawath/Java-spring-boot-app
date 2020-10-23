45a0c95901d49fedc9957f4455cc162b482d0e4c
bhagawath.m@soprasteria.com <bhagawath.m@soprasteria.com>
2020-10-23 16:50:32 +0530

    new

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Application {
 
-	@RequestMapping("/v4")
+	@RequestMapping("/v5")
 	public String home() {
 		return "{\"id\":\"2034\",\"projectName\":\"DEVOPS Accelerator\",\"companyName\":\"Sopra Steria\"}";
 	}
